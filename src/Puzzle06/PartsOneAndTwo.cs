0b05f01f6591368f0824883cc363a4c6f76d5cb4
Marco Thomazini <marco.thomazini@wendys.com>
2022-12-06 08:09:19 -0300

    Add Puzzle06

---
+++ src/Puzzle06/PartsOneAndTwo.cs
@@ -0,0 +1,65 @@
+﻿namespace Puzzle06
+{
+    internal static class PartsOneAndTwo
+    {
+        internal static void RunPartOne(string[] lines)
+        {
+            Run(lines, 4);
+        }
+
+        internal static void RunPartTwo(string[] lines)
+        {
+            Run(lines, 14);
+        }
+
+        private static void Run(string[] lines, int markerLength)
+        {
+            var charCounter = new int[256];
+            var marker = new char[markerLength];
+
+            foreach (var line in lines)
+            {
+                var latestIndex = 0;
+
+                var markerFound = false;
+
+                for (int lineIndex = 0; lineIndex <= line.Length - markerLength; lineIndex++)
+                {
+                    markerFound = true;
+
+                    for (int i = 0; i < marker.Length; i++)
+                    {
+                        var currentChar = line[lineIndex + i];
+                        charCounter[currentChar]++;
+                        marker[i] = currentChar;
+
+                        if (charCounter[currentChar] > 1)
+                        {
+                            markerFound = false;
+                        }
+                    }
+
+                    for (int i = 0; i < marker.Length; i++)
+                    {
+                        charCounter[marker[i]] = 0;
+                    }
+
+                    if (markerFound)
+                    {
+                        latestIndex = lineIndex + marker.Length;
+                        break;
+                    }
+                }
+
+                if (markerFound)
+                {
+                    Console.WriteLine($"How many characters need to be processed before the first start-of-packet marker is detected? {latestIndex}");
+                }
+                else
+                {
+                    Console.WriteLine($"No start-of-packet marker was detected :-(");
+                }
+            }
+        }
+    }
+}
---
+++ src/Puzzle06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Puzzle06
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var fileName = @"..\..\..\demo-input.txt";
+            //var fileName = @"..\..\..\demo-input-2.txt";
+            var fileName = @"..\..\..\input.txt";
+
+            var text = File.ReadAllText(fileName);
+
+            var lines = text.Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(">> PartOne.Run");
+            PartsOneAndTwo.RunPartOne(lines);
+
+            Console.WriteLine();
+
+            Console.WriteLine(">> PartTwo.Run");
+            PartsOneAndTwo.RunPartTwo(lines);
+        }
+    }
+}
